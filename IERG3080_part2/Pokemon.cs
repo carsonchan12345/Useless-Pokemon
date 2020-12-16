893042ebe4cbc8707ca067558cbca00f24ae5459
a11111ths <a11111ths@gmail.com>
2020-12-16 20:10:12 +0800

    some change

---
+++ IERG3080_part2/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IERG3080_part2
+{
+    class Pokemon
+    {
+        public Pokemon(int id)
+        {
+
+        }
+    }
+}
